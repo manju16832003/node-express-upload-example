07f8034b59655990ef4a2f83ffaa863d7506efa7
Manjunath Reddy <manju16832003@gmail.com>
2017-08-13 23:09:55 +0800

    Update unit tests

--- test/index.js
+++ test/index.js
@@ -1,10 +1,16 @@
-var expect = require('chai').expect;
-var request = require('request');
+var expect = require('chai').expect
+var request = require('request')
 
 describe('Video upload service home page', function () {
   it('I should see main page content', function () {
     request('http://localhost:3001', function (error, response, body) {
-      expect(body).to.equal('Woo Tag Video Upload API');
-    });
-  });
+      expect(body).to.equal('Woo Tag Video Upload API')
+    })
+  })
+
+  it('I should see main page status code is 200', function() {
+    request('http://localhost:8080' , function(error, response, body) {
+      expect(response.statusCode).to.equal(200)
+    })
+  })
 })
